ab4285da67334fe7c60744caf102d55ce1a90df1
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-08 16:16:37 -0300

    setup

---
+++ F1_News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  F1_News
+//
+//  Created by Marcelo Amaral Alves on 2024-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("F1 News App")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ F1_News/F1_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1_NewsApp.swift
+//  F1_News
+//
+//  Created by Marcelo Amaral Alves on 2024-04-07.
+//
+
+import SwiftUI
+
+@main
+struct F1_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
