c51d92fc7f434bdb9084428d2ca8e058c80ced03
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-08 16:25:00 -0300

    setup

--- F1_News/Views/ContentView.swift
+++ F1_News/Views/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelection = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("F1 News App")
+        TabView(selection: $tabSelection) {
+            NewsView()
+                .tag(1)
+            
+            SearchView()
+                .tag(2)
+            
+            SettingsView()
+                .tag(3)
         }
-        .padding()
     }
 }
 
