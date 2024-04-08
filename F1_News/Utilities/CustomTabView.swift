e768cf7caf63e57e1716f8e9ada2842a6c81d732
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-08 17:07:07 -0300

    CustomTabView

--- F1_News/Utilities/CustomTabView.swift
+++ F1_News/Utilities/CustomTabView.swift
@@ -1,18 +1,57 @@
-//
-//  CustomTabView.swift
-//  F1_News
-//
-//  Created by Marcelo Amaral Alves on 2024-04-08.
-//
-
 import SwiftUI
 
 struct CustomTabView: View {
+    @Binding var tabSelection: Int
+    @Namespace private var AnimationNamespace
+    
+    let tabBaritems: [(image: String, title: String)] = [("newspaper.fill", "News"), ("magnifyingglass", "Search"), ("gear", "Settings")]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Capsule()
+                .frame(height: 80)
+                .foregroundColor(Color(.secondarySystemBackground))
+                .shadow(radius: 2)
+            
+            HStack {
+                ForEach(0..<3) { index in
+                    Button {
+                        tabSelection = index + 1
+                    } label: {
+                        VStack(spacing: 8) {
+                            Spacer()
+                            
+                            Image(systemName: tabBaritems[index].image)
+                            
+                            Text(tabBaritems[index].title)
+                                .font(.caption)
+                            
+                            if index + 1 == tabSelection {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.red)
+                                    .matchedGeometryEffect(id: "SelectedTabID", in: AnimationNamespace)
+                                    .offset(y: 3)
+                            } else {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.clear)
+                                    .offset(y: 3)
+                            }
+                        }
+                        .foregroundColor(index + 1 == tabSelection ? .red : .gray)
+                    }
+                }
+            }
+            .frame(height: 80)
+            .clipShape(Capsule())
+        }
+        .padding(.horizontal)
     }
 }
 
-#Preview {
-    CustomTabView()
+struct CustomTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabView(tabSelection: .constant(1))
+    }
 }
