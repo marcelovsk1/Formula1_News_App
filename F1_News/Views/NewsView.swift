8023b8f8d3d430592b82a28a88495022b16f05e3
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-08 16:22:03 -0300

    setup the app

---
+++ F1_News/Views/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  F1_News
+//
+//  Created by Marcelo Amaral Alves on 2024-04-08.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}
