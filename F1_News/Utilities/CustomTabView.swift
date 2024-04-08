3b0880c38b32c52dc88c7c2e7cc879abdfe70a3f
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-08 16:29:06 -0300

    custom tab view

---
+++ F1_News/Utilities/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  F1_News
+//
+//  Created by Marcelo Amaral Alves on 2024-04-08.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabView()
+}
