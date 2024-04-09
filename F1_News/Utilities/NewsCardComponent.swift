267d379466e6a198be5d2433e180da84b031cb77
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-09 13:50:52 -0300

    Kingfisher

---
+++ F1_News/Utilities/NewsCardComponent.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCardComponent.swift
+//  F1_News
+//
+//  Created by Marcelo Amaral Alves on 2024-04-09.
+//
+
+import SwiftUI
+
+struct NewsCardComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsCardComponent()
+}
